f7f1bb2ab1e63e96111c866ab5242dfa701c0a51
khedoros <khedoros@gmail.com>
2016-10-21 13:54:56 -0700

    Cleaned up and translated main.c Added disassembly calls to dos_header.cpp

--- disasm/main.c
+++ disasm/main.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Демонстрационная оболочка
+ *  Demonstration shell
  * this is part of the libdisasm project
  *
  * author: Dmitry Podgorny <pasis.ua@gmail.com>
@@ -21,13 +21,13 @@ void usage(char *s) {
 
 int main(int argc, char **argv) {
 
-	char	s[64];		// строка (для вывода кода)
-	uint8	*code,		// для загрузки программы
-		*index,		// указатель на текущую команду
-		*tmp;		// для временного хранения указателя
-	int	len,		// размер файла
-		ifindex = 0,	// индекс имени для чтения
-		ofindex = 0,	// индекс имени для записи
+	char	s[64];		// String (for output code)
+	uint8	*code,		// to hold the binary itself
+		*index,		// pointer to the current command
+		*tmp;		// pointer to temporary storage
+	int	len,		// file size
+		ifindex = 0,	// index in argv of the input file
+		ofindex = 0,	// index in argv of the output file
 		i;
 	uint	bit = USE32,
 		offset = 0;
@@ -36,23 +36,23 @@ int main(int argc, char **argv) {
 
 	//=====================================================================
 
-	i = 1;		// индекс в argv
-	while ( i < argc ) {
+	i = 1;		// index in argv
+	while ( i < argc ) { //Process arguments in argv
 
-		if ( !strcmp(argv[i], "--help") ) {
+		if ( !strcmp(argv[i], "--help") ) {//Print use, exit
 			usage(argv[0]);
 			return 0;
 		} else
 
-		if ( !strcmp(argv[i], "-i") ) {
-			if ( i == argc - 1 ) {
+		if ( !strcmp(argv[i], "-i") ) { //Specify input file
+			if ( i == argc - 1 ) { //If -i is the last option, print usage, because the filename argument is missing
 				usage(argv[0]);
 				return 2;
 			}
 			ifindex = ++i;
 		} else
 
-		if ( !strcmp(argv[i], "-o") ) {
+		if ( !strcmp(argv[i], "-o") ) { //Specify output file
 			if ( i == argc - 1 ) {
 				usage(argv[0]);
 				return 2;
@@ -61,18 +61,21 @@ int main(int argc, char **argv) {
 		} else
 
 		if ( !strcmp(argv[i], "--use16") )
+		    printf(" use16\n");
 			bit = USE16;
 		else
 		if ( !strcmp(argv[i], "--use32") )
+		    printf(" use32\n");
 			bit = USE32;
 		else
 
-		if ( !strcmp(argv[i], "--org") ) {
+		if ( !strcmp(argv[i], "--org") ) { //Specify starting offset to read from the file
 			if ( i == argc - 1 ) {
 				usage(argv[0]);
 				return 2;
 			}
 			offset = atoi(argv[++i]);
+	        printf(" org 0x%x\n\n", offset);
 		}
 
 		else {
@@ -86,43 +89,42 @@ int main(int argc, char **argv) {
 
 	//=====================================================================
 
+    //Open the input and output files, get the size of the input, allocate space to store it
+
 	if ( ( f = fopen(argv[ifindex], "rb") ) == NULL ) {
 		fprintf(stderr, "error while opening file \"%s\"!\n", argv[ifindex]);
 		return 2;
 	}
 
-	if ( ofindex )
+	if ( ofindex ) //output file is optional
 		if ( ( of = freopen(argv[ofindex], "w", stdout) ) == NULL ) {
 			fprintf(stderr, "error while creating file \"%s\"!\n", argv[ofindex]);
 			fclose(f);
 			return 2;
 		}
 
+    //get binary filesize
 	fseek(f, 0, SEEK_END);
-	len = ftell(f);				// размер файла
+	len = ftell(f);
 	fseek(f, 0, SEEK_SET);
 
-	index = code = (uint8 *) malloc(len + 8);// с запасом )))
 
-	fread(code, 1, len, f);
+	index = code = (uint8 *) malloc(len + 8);// with a "reserve" )))
+
+	fread(code, 1, len, f); //read "len" bytes from "f" to "code"
 	fclose(f);
 
 	//=====================================================================
 
-	switch ( bit ) {
-	case USE16:
-		printf(" use16\n");
-		break;
-	case USE32:
-	default:
-		printf(" use32\n");
-		break;
-	}
+    //index: current position in binary. 
+    //code:  pointer to beginning of binary
+    //len:   length of binary data
+    
+	while ( (uint) index < (uint) code + len ) { //While eof hasn't been reached:
 
-	printf(" org 0x%x\n\n", offset);
-
-	while ( (uint) index < (uint) code + len ) {
-
+        //s: output string buffer
+        //bit: expected bitness of code
+        //offset + ((uint...: current offset within the binary
 		tmp = disasm(index, s, bit, offset + ( (uint) index - (uint) (code) ));
 		if ( !tmp ) {
 			fprintf(stderr, "error: unknown opcode!\nstring: 0x%x 0x%x 0x%x 0x%x 0x%x\n", *index++, *index++, *index++, *index++, *index);
--- dos_header.cpp
+++ dos_header.cpp
@@ -3,6 +3,12 @@
 #include<stdint.h>
 #include<assert.h>
 #include<vector>
+
+extern "C" {
+#include "disasm/disasm.h"
+#include "disasm/string.h"
+}
+
 using namespace std;
 
 typedef struct {
@@ -24,32 +30,65 @@ typedef struct {
 } mz_header;
 
 int main(int argc, char *argv[]) {
+    //Get the filesize
     assert(sizeof(mz_header) == 32);
     ifstream in;
     in.open(argv[1]);
     in.seekg(0,ios::end);
     size_t size = in.tellg();
+
+    //Read the header
     in.seekg(0,ios::beg);
     cout<<"Filename: "<<argv[1]<<endl<<"Filesize: "<<size<<endl;
     mz_header h;
     in.read(reinterpret_cast<char *>(&h), sizeof(mz_header));
+
+    //Read the relocation table
     vector<uint16_t> reloc_table;
     reloc_table.resize(h.reloc_entries*2);
     in.seekg(h.reloc_offset,ios::beg);
     in.read(reinterpret_cast<char *>(&(reloc_table[0])), h.reloc_entries*4);
-    printf("Bytes on last page: %04x\nTotal pages: %04x\nRelocation entries: %d\nHeader size in paragraphs: %04x\nMin mem needed after exe: %04x\nMax mem needed after exe: %04x\nSS offset: %04x\nInitial SP: %04x\nChecksum: %04x\nEntry point: %04x:%04x\nOffset of relocation table: %04x\nOverlay number (expect 0): %04x\nCould be ID: %02x %02x %02x %02x\n",h.last_page_bytes,h.total_pages,h.reloc_entries,h.header_para_size,h.min_paras_after_code,h.max_paras_after_code,h.stack_seg_offset,h.initial_sp,h.chksum,h.init_cs,h.init_ip,h.reloc_offset,h.overlay_num,h.poss_id[0],h.poss_id[1],h.poss_id[2],h.poss_id[3]);
+
+    //Output information gathered about the binary
+    printf("Bytes on last page: 0x%04x\nTotal pages: 0x%04x\nRelocation entries: %d\nHeader size in paragraphs: 0x%04x\nMin mem needed after exe: 0x%04x\nMax mem needed after exe: 0x%04x\nSS offset: 0x%04x\nInitial SP: 0x%04x\nChecksum: 0x%04x\nEntry point: %04x:%04x\nOffset of relocation table: 0x%04x\nOverlay number (expect 0): 0x%04x\nCould be ID: %02x %02x %02x %02x\n",h.last_page_bytes,h.total_pages,h.reloc_entries,h.header_para_size,h.min_paras_after_code,h.max_paras_after_code,h.stack_seg_offset,h.initial_sp,h.chksum,h.init_cs,h.init_ip,h.reloc_offset,h.overlay_num,h.poss_id[0],h.poss_id[1],h.poss_id[2],h.poss_id[3]);
     if(h.poss_id[0] == 0x01 && h.poss_id[1] == 0x00 && h.poss_id[2] == 0xfb) {
         std::cout<<"Probably Borland TLink, version "<<h.poss_id[3]/16<<endl;
     }
     cout<<"Relocation table: "<<h.reloc_entries<<" entries"<<endl;
-    bool odd=false;
-    uint16_t offset=0;
-    for(uint16_t off:reloc_table) {
-        if(odd)
-            printf("%04x:%04x\n",off,offset);
-        else
-            offset = off;
-        odd=!odd;
+    {
+        bool odd=false;
+        uint16_t offset=0;
+        for(uint16_t off:reloc_table) {
+            if(odd)
+                printf("%04x:%04x\n",off,offset);
+            else
+                offset = off;
+            odd=!odd;
+        }
     }
+
+    //Read the rest of the contents of the binary
+    vector<uint8_t> bin(size - h.header_para_size * 0x10,0);
+    in.seekg(h.header_para_size * 0x10, ios::beg);
+    in.read(reinterpret_cast<char *>(&bin[0]), bin.size());
+
+    uint8_t * cur_data = &bin[0];
+    uint8_t * base_data = &bin[0];
+    uint8_t * out_dat_buf = NULL;
+    size_t offset = h.header_para_size * 0x10;
+    uint32_t bitness = USE16;
+    char outbuff[64] = {0};
+    
+    while(cur_data < base_data + size) { //while EOF hasn't been reached
+       printf("%02x %02x %02x %02x %02x  ", *cur_data, *(cur_data+1),*(cur_data+2),*(cur_data+3),*(cur_data+4));
+       out_dat_buf = disasm((uint8 *)cur_data, outbuff, bitness, offset + ( cur_data - base_data) );
+       if ( !out_dat_buf ) {
+           fprintf(stderr, "error: unknown opcode!\nstring: 0x%x 0x%x 0x%x 0x%x 0x%x\n", *cur_data++, *cur_data++, *cur_data++, *cur_data++, *cur_data);
+           return 1;
+       }
+       printf("%s\n", outbuff);
+       cur_data = out_dat_buf;
+    }
+
     return 0;
 }
